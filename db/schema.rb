d9650452918d2deb1b1835c149eb45374a94e198
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-02-20 16:00:04 +0900

    カラム追加
    rails db:migrate:status
    でUPを確認。これだと編集できない。
    rails db:rollback
    rails db:migrate:status
    でDOWNになってるのを確認。
    追加するカラムを記入。
    rails db:migrate
    で完了。

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_20_051651) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
