676ddacf9fdf54468d96b296388d4a7b1ca62c66
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-02-22 12:22:07 +0900

    ログアウト途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'prototypes', to: 'prototypes#index' 
+
+  as :user do
+    delete '/users/sign_out', to: 'devise/sessions#destroy', as: :logout
+  end
+
+  get 'prototypes', to: 'prototypes#index'
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
   root to: "prototypes#index"
 end
